5997f5b84e09238457477dbfc528db647a4aa249
Patricio <patricioarredondop@gmail.com>
2019-08-21 16:25:21 -0400

    Base Project

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+
+Route::get('/', 'TestController@welcome');
+
+ Route::get('/prueba', function (){
+    return 'Hola soy una ruta de prueba';
+
+ });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
